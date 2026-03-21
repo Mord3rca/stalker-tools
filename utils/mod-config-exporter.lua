628547c1da488f95e6104a472ae88cc3c7df3bf9
Mord3rca <morderca@morderca.net>
2026-03-21 12:14:03 +0100

    utils: Adding a bit more animation key for reloading

--- utils/mod-config-exporter.lua
+++ utils/mod-config-exporter.lua
@@ -9,7 +9,32 @@ end
 
 function _adding_motion_length(section)
     local anim_key_name = {
-        "anm_reload"
+        "anm_reload", "anm_reload_1", "anm_reload_2",
+        "anm_reload_aim", "anm_reload_aim_ammo1", "anm_reload_aim_ammo11",
+        "anm_reload_aim_ammo13", "anm_reload_aim_ammo15",
+        "anm_reload_aim_ammo17", "anm_reload_aim_ammo19", "anm_reload_aim_ammo2",
+        "anm_reload_aim_ammo21", "anm_reload_aim_ammo23", "anm_reload_aim_ammo25",
+        "anm_reload_aim_ammo27", "anm_reload_aim_ammo29", "anm_reload_aim_ammo3",
+        "anm_reload_aim_ammo31", "anm_reload_aim_ammo4", "anm_reload_aim_ammo5",
+        "anm_reload_aim_ammo6", "anm_reload_aim_ammo7", "anm_reload_aim_ammo8",
+        "anm_reload_aim_ammo9", "anm_reload_ammo1", "anm_reload_ammo11",
+        "anm_reload_ammo13", "anm_reload_ammo15", "anm_reload_ammo17",
+        "anm_reload_ammo19", "anm_reload_ammo2", "anm_reload_ammo21",
+        "anm_reload_ammo23", "anm_reload_ammo25", "anm_reload_ammo27",
+        "anm_reload_ammo29", "anm_reload_ammo3", "anm_reload_ammo31",
+        "anm_reload_ammo4", "anm_reload_ammo5", "anm_reload_ammo6",
+        "anm_reload_ammo7", "anm_reload_ammo8", "anm_reload_ammo9",
+        "anm_reload_empty", "anm_reload_empty_aim", "anm_reload_empty_end",
+        "anm_reload_empty_no_mag", "anm_reload_empty_variant1", "anm_reload_empty_w_gl",
+        "anm_reload_empty_w_gl_aim", "anm_reload_empty_w_gl_variant1", "anm_reloadendemptytac_end",
+        "anm_reloadendtac_end", "anm_reloadendtacnachalo_end", "anm_reload_g",
+        "anm_reload_misfire", "anm_reload_misfire_aim", "anm_reload_misfire_aim_jammed",
+        "anm_reload_misfire_empty", "anm_reload_misfire_empty_aim", "anm_reload_misfire_jammed",
+        "anm_reload_misfire_variant1", "anm_reload_misfire_variant2", "anm_reload_misfire_variant3",
+        "anm_reload_misfire_variant4", "anm_reload_misfire_variant5", "anm_reload_misfire_variant6",
+        "anm_reload_misfire_w_gl", "anm_reload_misfire_w_gl_aim", "anm_reload_variant1",
+        "anm_reload_w_gl", "anm_reload_w_gl_aim", "anm_reload_w_gl_misfire",
+        "anm_reload_w_gl_variant1",
     } -- Can be extended for your needs
     -- To get all available anm key exec: grep -Eo 'anm_[^= ]*' outfile.ini | sort -u
 
