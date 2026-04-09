9c0d83d8824801eebfd3004a4e04c3b5c4cd40b4
Mord3rca <morderca@morderca.net>
2026-04-09 13:09:54 +0200

    dynarray: API change to allow argument passing for callbacks function

--- include/dynarray.h
+++ include/dynarray.h
@@ -13,14 +13,14 @@ struct dynarray *dynarray_create(int);
 void free_dynarray(struct dynarray*, void (*)(void*));
 
 // Iterator
-void dynarray_foreach(struct dynarray*, bool (*)(void*));
+void dynarray_foreach(struct dynarray*, bool (*)(void*, void*), void*);
 
 // Member manupulation
 int dynarray_insert(struct dynarray*, void*);
 int dynarray_remove(struct dynarray*, void*);
 
 // Search
-void *dynarray_find(struct dynarray*, bool (*)(void*));
+void *dynarray_find(struct dynarray*, bool (*)(void*, void*), void*);
 size_t dynarray_find_member_index(struct dynarray*, void*);
 
 #endif //DYNAMIC_ARRAY_HEADER
