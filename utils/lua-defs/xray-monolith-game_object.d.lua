610e5f166047f25f289c9dd8c793829436bbd60a
Mord3rca <morderca@morderca.net>
2026-04-14 21:15:07 +0200

    lua-defs: Adding alife definitions

---
+++ utils/lua-defs/xray-monolith-game_object.d.lua
@@ -0,0 +1,11 @@
+---@meta
+
+-- Exported in src/xrGame/script_game_object_script2.cpp
+
+---@enum EPathType
+EPathType = {
+    game_path = 0,
+    level_path = 1,
+    patrol_path = 2,
+    no_path = 3,
+}
---
+++ utils/lua-defs/xray-monolith-server_objects.d.lua
@@ -0,0 +1,6 @@
+---@meta
+
+-- Exported in src/xrServerEntities/xrServer_Objects_script.cpp
+
+---@class cse_abstract
+cse_abstract = {}
