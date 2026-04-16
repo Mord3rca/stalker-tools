ba4f5a5d21d00c74ff87267ea6de22e6d14025a3
Mord3rca <morderca@morderca.net>
2026-04-16 19:20:43 +0200

    workflows: Adding reference file

--- .github/scripts/basic-utest.sh
+++ .github/scripts/basic-utest.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+[ -v V ] && set -x
+
 set -eu -o pipefail
 
-test_file="${1}"
-test_hash="${2}"
+ifile="${1}"
+wfile="$(mktemp --tmpdir dltx-reader-out-XXXXXX)"
+trap 'rm "${wfile}"' EXIT
 
-read hash _ < <(valgrind --show-error-list=yes --error-exitcode=1 --leak-check=full out/dltx-reader "tests/data/${test_file}" | md5sum -)
+valgrind --show-error-list=yes --error-exitcode=1 --leak-check=full out/dltx-reader "tests/data/${ifile}" >"${wfile}"
 
-[ "${hash}" == "${test_hash}" ] || {
+read -r ihash _ < <(md5sum "tests/expected/${ifile}")
+read -r whash _ < <(md5sum "${wfile}")
+
+[ "${ihash}" == "${whash}" ] || {
 	echo "Hash did not match, aborting"
+	diff "tests/expected/${ifile}" "${wfile}"
 	exit 1
 } >&2
