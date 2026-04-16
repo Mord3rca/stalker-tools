a31badd804fd207feb5712501fa3686fe5610825
Mord3rca <morderca@morderca.net>
2026-04-16 19:29:40 +0200

    workflow: Modify the test script to run on all files if 0 args are provided

--- .github/scripts/basic-utest.sh
+++ .github/scripts/basic-utest.sh
@@ -4,17 +4,29 @@
 
 set -eu -o pipefail
 
-ifile="${1}"
 wfile="$(mktemp --tmpdir dltx-reader-out-XXXXXX)"
 trap 'rm "${wfile}"' EXIT
 
-valgrind --show-error-list=yes --error-exitcode=1 --leak-check=full out/dltx-reader "tests/data/${ifile}" >"${wfile}"
+test_file() {
+	ifile="${1}"
 
-read -r ihash _ < <(md5sum "tests/expected/${ifile}")
-read -r whash _ < <(md5sum "${wfile}")
+	valgrind --show-error-list=yes --error-exitcode=1 --leak-check=full out/dltx-reader "tests/data/${ifile}" >"${wfile}"
 
-[ "${ihash}" == "${whash}" ] || {
-	echo "Hash did not match, aborting"
-	diff "tests/expected/${ifile}" "${wfile}"
-	exit 1
-} >&2
+	read -r ihash _ < <(md5sum "tests/expected/${ifile}")
+	read -r whash _ < <(md5sum "${wfile}")
+
+	[ "${ihash}" == "${whash}" ] || {
+		echo "Hash did not match, aborting"
+		diff "tests/expected/${ifile}" "${wfile}"
+		exit 1
+	} >&2
+}
+
+if [ "${#}" -eq 1 ]; then
+	test_file "${@}"
+else
+	for f in tests/data/*.ltx; do
+		echo "--- TESTING ${f} ---"
+		test_file "$(basename "${f}")"
+	done
+fi
