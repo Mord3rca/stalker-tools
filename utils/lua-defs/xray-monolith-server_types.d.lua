076332d94cc1eb18553e777f143bb04e914c9ea2
Mord3rca <morderca@morderca.net>
2026-04-26 11:13:26 +0200

    lua-defs: Adding missing defs of xrGame game sound types without docs

---
+++ utils/lua-defs/xray-monolith-server_types.d.lua
@@ -0,0 +1,56 @@
+---@meta
+
+-- Exported in src\xrServerEntities\script_sound_type_script.cpp
+
+---@class snd_type
+snd_type = {}
+
+---@enum snd_type.sound_types
+snd_type.sound_types = {
+    no_sound = 0,
+    weapon = 1,
+    item = 2,
+    monster = 3,
+    anomaly = 4,
+    world = 5,
+    pick_up = 6,
+    drop = 7,
+    hide = 8,
+    take = 9,
+    use = 10,
+    shoot = 11,
+    empty = 12,
+    bullet_hit = 13,
+    reload = 14,
+    die = 15,
+    injure = 16,
+    step = 17,
+    talk = 18,
+    attack = 19,
+    eat = 20,
+    idle = 21,
+    object_break = 22,
+    object_collide = 23,
+    object_explode = 24,
+    ambient = 25,
+    item_pick_up = 26,
+    item_drop = 27,
+    item_hide = 28,
+    item_take = 29,
+    item_use = 30,
+    weapon_shoot = 31,
+    weapon_empty = 32,
+    weapon_bullet_hit = 33,
+    weapon_reload = 34,
+    monster_die = 35,
+    monster_injure = 36,
+    monster_step = 37,
+    monster_talk = 38,
+    monster_attack = 39,
+    monster_eat = 40,
+    anomaly_idle = 41,
+    world_object_break = 42,
+    world_object_collide = 43,
+    world_object_explode = 44,
+    world_ambient = 45
+} -- TODO: Check values
