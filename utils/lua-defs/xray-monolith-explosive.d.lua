d4711dd60b86ad265fec9154a35ad75956bd0df1
Mord3rca <morderca@morderca.net>
2026-04-26 13:36:14 +0200

    lua-defs: Bulk import without docs #1

---
+++ utils/lua-defs/xray-monolith-explosive.d.lua
@@ -0,0 +1,12 @@
+---@meta
+
+-- Exposed in src\xrGame\ExplosiveScript.cpp
+
+---@class explosive
+explosive = {}
+
+function explosive:explode() end
+function explosive:Initiator() end
+function explosive:SetInitiator() end
+function explosive:LoadExplosiveSection() end
+function explosive:LoadExplosiveSection() end
