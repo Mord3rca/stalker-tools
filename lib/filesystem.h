950bb81648ea9ae4ff214b71d19df9201b19868b
Mord3rca <morderca@morderca.net>
2026-04-28 17:18:52 +0200

    fs: Moving all FS related functions to a separate file

---
+++ lib/filesystem.h
@@ -0,0 +1,16 @@
+#ifndef _FILESSYTEM_HEADER
+#define _FILESSYTEM_HEADER
+
+typedef enum {
+	FS_NO_ERROR = 0,
+	FS_GLOB_ERROR,
+	FS_GLOB_NO_MATCH,
+
+	FS_GENERIC_ERROR,
+} fs_return_code;
+
+char *filesystem_path_append(const char[], const char[]);
+fs_return_code filesystem_glob(const char[], const char[], char ***);
+char *filesystem_get_modfile_glob_path(const char[]);
+
+#endif  // _FILESSYTEM_HEADER
