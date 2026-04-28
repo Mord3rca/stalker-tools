17b5d941002cd125dcc167fb8695477728148899
Mord3rca <morderca@morderca.net>
2026-04-28 17:37:45 +0200

    dltx_parser: tolower() on inclusion path ... Will work for now

--- lib/filesystem.h
+++ lib/filesystem.h
@@ -12,5 +12,6 @@ typedef enum {
 char *filesystem_path_append(const char[], const char[]);
 fs_return_code filesystem_glob(const char[], const char[], char ***);
 char *filesystem_get_modfile_glob_path(const char[]);
+void filesystem_path_tolower(char[]);
 
 #endif  // _FILESSYTEM_HEADER
