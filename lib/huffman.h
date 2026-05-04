8033849858853b8dd579973b7abd6d84b0c696e6
Mord3rca <morderca@morderca.net>
2026-05-04 15:52:42 +0200

    huffman: Expose compress/decompress function to rest of the code
    & make huffman.c compilable

---
+++ lib/huffman.h
@@ -0,0 +1,7 @@
+#ifndef _HUFFMAN_HEADER
+#define _HUFFMAN_HEADER
+
+void huffman_compress(void**, size_t*, void*, size_t);
+void huffman_decompress(void**, size_t*, void*, size_t);
+
+#endif  // _HUFFMAN_HEADER
