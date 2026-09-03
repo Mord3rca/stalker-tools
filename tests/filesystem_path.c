139944a7d50480a262107af4eb9c75ff2102c55d
Mord3rca <morderca@morderca.net>
2026-09-03 10:45:12 +0200

    tests: Unit tests for **stcore_filesystem_path**

--- tests/filesystem_path.c
+++ tests/filesystem_path.c
@@ -5,7 +5,9 @@
 
 static const char t[] = "/path/to/a/file";
 
+char *path_canonicalize_test;
 stcore_filesystem_path *path_create_test;
+stcore_filesystem_path *path_sys_create_test;
 
 enum {
 	IDX_PATH1 = 0,
@@ -17,7 +19,12 @@ enum {
 	IDX_PATH2_DIRNAME,
 
 	IDX_PATH3,
+	IDX_PATH3_BASENAME,
+	IDX_PATH3_DIRNAME,
+
 	IDX_PATH4,
+	IDX_PATH4_BASENAME,
+	IDX_PATH4_DIRNAME,
 
 	IDX_PATH_APPEND1,
 	IDX_PATH_APPEND2,
@@ -39,7 +46,7 @@ static void test_fs_path_init(void)
 	CU_ASSERT_EQUAL(path.len, 15);
 	CU_ASSERT_EQUAL(path.scount, 4);
 	CU_ASSERT_EQUAL(path.last_sep_offset, 10);
-	CU_ASSERT_EQUAL(strncmp(path.target, t, strlen(path.target)), 0);
+	CU_ASSERT_EQUAL(strncmp(path.target, t, strlen(t)), 0);
 }
 
 static void test_fs_path_create(void)
@@ -58,31 +65,48 @@ static void test_fs_path_copy(void)
 {
 	stcore_filesystem_path p;
 
-	stcore_filesystem_path_copy(paths[IDX_PATH1], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1]));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH1_BASENAME], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1_BASENAME]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1_BASENAME]));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH1_DIRNAME], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1_DIRNAME]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1_DIRNAME]));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH2], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH2]));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH2_BASENAME], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2_BASENAME]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH2_BASENAME]));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH2_DIRNAME], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2_DIRNAME]);
 	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH2_DIRNAME]));
 }
 
 static void test_fs_path_cmp(void)
 {
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH1], paths[IDX_PATH2]) > 0);
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH1], paths[IDX_PATH1_BASENAME]) > 0);
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH1], paths[IDX_PATH1_DIRNAME]) > 0);
+
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH2], paths[IDX_PATH4]) > 0);
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH2], paths[IDX_PATH2_BASENAME]) > 0);
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH2], paths[IDX_PATH2_DIRNAME]) > 0);
+
+	CU_ASSERT(stcore_filesystem_path_cmp(paths[IDX_PATH4], paths[IDX_PATH2]) < 0);
+
 }
 
 static void test_fs_path_equal(void)
 {
+	stcore_filesystem_path p;
+
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH1], paths[IDX_PATH1]));
+	CU_ASSERT(!stcore_filesystem_path_equal(paths[IDX_PATH1], paths[IDX_PATH2]));
+
+	stcore_filesystem_path_init(&p, paths[IDX_PATH2].target);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH2], p));
 }
 
 static void test_fs_path_is_absolute(void)
@@ -96,56 +120,160 @@ static void test_fs_path_is_absolute(void)
 	CU_ASSERT(!stcore_filesystem_path_is_absolute(paths[IDX_PATH2_DIRNAME]));
 
 	CU_ASSERT(!stcore_filesystem_path_is_absolute(paths[IDX_PATH3]));
+	CU_ASSERT(!stcore_filesystem_path_is_absolute(paths[IDX_PATH3_BASENAME]));
+	CU_ASSERT(!stcore_filesystem_path_is_absolute(paths[IDX_PATH3_DIRNAME]));
 
 	CU_ASSERT(stcore_filesystem_path_is_absolute(paths[IDX_PATH4]));
+	CU_ASSERT(!stcore_filesystem_path_is_absolute(paths[IDX_PATH4_BASENAME]));
+	CU_ASSERT(stcore_filesystem_path_is_absolute(paths[IDX_PATH4_DIRNAME]));
 }
 
 static void test_fs_path_dirname(void)
 {
 	stcore_filesystem_path p;
 
-	stcore_filesystem_path_dirname(paths[IDX_PATH1], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1]);
+	stcore_filesystem_path_dirname(&p);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH1_DIRNAME], p));
 
-	stcore_filesystem_path_dirname(paths[IDX_PATH2], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2]);
+	stcore_filesystem_path_dirname(&p);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH2_DIRNAME], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH3]);
+	stcore_filesystem_path_dirname(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH3_DIRNAME], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH4]);
+	stcore_filesystem_path_dirname(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH4_DIRNAME], p));
 }
 
 static void test_fs_path_basename(void)
 {
 	stcore_filesystem_path p;
 
-	stcore_filesystem_path_basename(paths[IDX_PATH1], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1]);
+	stcore_filesystem_path_basename(&p);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH1_BASENAME], p));
 
-	stcore_filesystem_path_basename(paths[IDX_PATH2], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2]);
+	stcore_filesystem_path_basename(&p);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH2_BASENAME], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH3]);
+	stcore_filesystem_path_basename(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH3_BASENAME], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH4]);
+	stcore_filesystem_path_basename(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH4_BASENAME], p));
 }
 
 static void test_fs_path_append(void)
 {
 	stcore_filesystem_path p;
 
-	stcore_filesystem_path_copy(paths[IDX_PATH2], &p);
-	stcore_filesystem_path_append(paths[IDX_PATH4], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2]);
+	stcore_filesystem_path_append(&p, paths[IDX_PATH4]);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_APPEND1], p));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH3], &p);
-	stcore_filesystem_path_append(paths[IDX_PATH1], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH3]);
+	stcore_filesystem_path_append(&p, paths[IDX_PATH1]);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_APPEND2], p));
+}
+
+static void test_fs_path_append2(void)
+{
+	stcore_filesystem_path p, q;
+
+	stcore_filesystem_path_init(&q, "/path/to/home");
+	stcore_filesystem_path_append2(&p, paths[IDX_PATH1].target, paths[IDX_PATH3].target);
+	CU_ASSERT(stcore_filesystem_path_equal(p, q));
 }
 
 static void test_fs_path_prepend(void)
 {
 	stcore_filesystem_path p;
 
-	stcore_filesystem_path_copy(paths[IDX_PATH1_BASENAME], &p);
-	stcore_filesystem_path_prepend(paths[IDX_PATH4], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1_BASENAME]);
+	stcore_filesystem_path_prepend(&p, paths[IDX_PATH4]);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_PREPEND1], p));
 
-	stcore_filesystem_path_copy(paths[IDX_PATH2_DIRNAME], &p);
-	stcore_filesystem_path_prepend(paths[IDX_PATH1], &p);
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2_DIRNAME]);
+	stcore_filesystem_path_prepend(&p, paths[IDX_PATH1]);
 	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_PREPEND2], p));
+}
+
+static void test_fs_path_append_str(void)
+{
+	stcore_filesystem_path p;
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2]);
+	stcore_filesystem_path_append_str(&p, paths[IDX_PATH4].target);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_APPEND1], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH3]);
+	stcore_filesystem_path_append_str(&p, paths[IDX_PATH1].target);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_APPEND2], p));
+}
+
+static void test_fs_path_prepend_str(void)
+{
+	stcore_filesystem_path p;
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH1_BASENAME]);
+	stcore_filesystem_path_prepend_str(&p, paths[IDX_PATH4].target);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_PREPEND1], p));
+
+	stcore_filesystem_path_copy(&p, paths[IDX_PATH2_DIRNAME]);
+	stcore_filesystem_path_prepend_str(&p, paths[IDX_PATH1].target);
+	CU_ASSERT(stcore_filesystem_path_equal(paths[IDX_PATH_PREPEND2], p));
+}
+
+static void test_fs_path_to_system(void)
+{
+	stcore_filesystem_path p;
+
+	stcore_filesystem_path_init(&p, "relative\\path");
+	stcore_filesystem_path_to_system(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH2]));
+}
+
+static void test_fs_path_to_lower(void)
+{
+	stcore_filesystem_path p;
+
+	stcore_filesystem_path_init(&p, "/PaTH/TO/File");
+	stcore_filesystem_path_to_lower(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1]));
+}
+
+static void test_fs_path_create_to_system(void)
+{
+	path_sys_create_test = stcore_filesystem_path_create_to_system("\\path\\to\\file");
+	CU_ASSERT(stcore_filesystem_path_equal(*path_sys_create_test, paths[IDX_PATH1]));
+}
+
+static void test_fs_path_canonicalize(void)
+{
+	stcore_filesystem_path p;
+
+	stcore_filesystem_path_init(&p, "//./path//to/file//");
+	stcore_filesystem_path_canonicalize(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1]));
+
+	stcore_filesystem_path_init(&p, "./file");
+	stcore_filesystem_path_canonicalize(&p);
+	CU_ASSERT(stcore_filesystem_path_equal(p, paths[IDX_PATH1_BASENAME]));
+}
+
+static void test_fs_path_canonicalize_str(void)
+{
+	char p[] = "//./path/to/file//.//";
+
+	path_canonicalize_test = stcore_filesystem_path_canonicalize_str(p);
+	CU_ASSERT(strcmp(path_canonicalize_test, paths[IDX_PATH1].target) == 0);
 }
 
 // Exported functions
@@ -161,7 +289,12 @@ int fs_path_init(void)
 	stcore_filesystem_path_init(paths + IDX_PATH2_DIRNAME, "relative");
 
 	stcore_filesystem_path_init(paths + IDX_PATH3, "home");
+	stcore_filesystem_path_init(paths + IDX_PATH3_BASENAME, "home");
+	stcore_filesystem_path_init(paths + IDX_PATH3_DIRNAME, ".");
+
 	stcore_filesystem_path_init(paths + IDX_PATH4, "/root");
+	stcore_filesystem_path_init(paths + IDX_PATH4_BASENAME, "root");
+	stcore_filesystem_path_init(paths + IDX_PATH4_DIRNAME, "/");
 
 	stcore_filesystem_path_init(paths + IDX_PATH_APPEND1, "relative/path/root");
 	stcore_filesystem_path_init(paths + IDX_PATH_APPEND2, "home/path/to/file");
@@ -173,7 +306,9 @@ int fs_path_init(void)
 
 int fs_path_cleanup(void)
 {
+	free(path_canonicalize_test);
 	free(path_create_test);
+	free(path_sys_create_test);
 	return 0;
 }
 
@@ -191,7 +326,18 @@ CU_TestInfo fs_path_tests[] = {
 	{"basename", test_fs_path_basename},
 
 	{"append", test_fs_path_append},
+	{"append2", test_fs_path_append2},
 	{"prepend", test_fs_path_prepend},
 
+	{"append_str", test_fs_path_append_str},
+	{"prepend_str", test_fs_path_prepend_str},
+
+	{"to_system", test_fs_path_to_system},
+	{"to_lower", test_fs_path_to_lower},
+
+	{"create_to_system", test_fs_path_create_to_system},
+
+	{"canonicalize", test_fs_path_canonicalize},
+	{"canonicalize_str", test_fs_path_canonicalize_str},
 	CU_TEST_INFO_NULL,
 };
